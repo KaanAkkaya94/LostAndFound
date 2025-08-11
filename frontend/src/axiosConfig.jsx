c2413df93f90b89881ad7bd6b1dd2f3755cde97c
KaanAkkaya <150163708+KaanAkkaya94@users.noreply.github.com>
2025-08-12 09:43:18 +1000

    updated IP on axios Live

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.6.247:5001', // live
+  baseURL: 'http://52.65.19.41:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
